b821697c671736a0a52146dac874787fb02e7eed
Pavlo Kysliak <pavel.kis0505@gmail.com>
2022-11-15 13:18:33 +0200

    children

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child.component.scss']
 })
 export class ChildComponent implements OnInit {
+  // @ContentChild('children') children: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+    // console.log(this.children)
   }
 
 }
