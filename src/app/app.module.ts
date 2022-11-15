977f364d5706fd190b2f12b6bff7f44a780e5344
Pavlo Kysliak <pavel.kis0505@gmail.com>
2022-11-15 11:04:01 +0200

    children

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { NumberComponent } from './number/number.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
-    NumberComponent
+    NumberComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule
