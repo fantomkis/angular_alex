9b43b68331e9599815bba01f1c439a5d2547818b
Pavlo Kysliak <pavel.kis0505@gmail.com>
2023-01-03 10:45:51 +0200

    Update

--- src/app/directive/color.directive.ts
+++ src/app/directive/color.directive.ts
@@ -1,13 +1,20 @@
 
-import { Directive, ElementRef, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from "@angular/core";
 
 @Directive({
     selector:'[appColor]'
 })
+   
 export class ColorDirective{
+      @Input('appColor') color:string = 'blue'
+   
     constructor(private el: ElementRef, private r: Renderer2) {
         // this.r.setStyle(this.el.nativeElement,  style: 'color', value: 'red', )
         el.nativeElement.style.color = 'red'
+    }
+    @HostListener('mouseenter') onEvent() {
+
+    //  this.r.setStyle(this.el.nativeElement )
     }
 
 }
